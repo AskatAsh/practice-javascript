6b92da228b9b74abfb158a759115d13683690c76
AskatAsh <abdulaskat@gmail.com>
2022-01-26 23:21:55 +0600

    js mathematical operation learned

---
+++ math-operation.js
@@ -0,0 +1,18 @@
+var boroiPrice = 120;
+var khiraPrice = 40;
+
+// mathematical operation addition and substraction
+// console.log(boroiPrice+khiraPrice);
+// console.log(boroiPrice-khiraPrice);
+
+// var totalPrice = boroiPrice + khiraPrice;
+// console.log(totalPrice);
+
+// multiplication in js
+// var khiraQuantityinKg = 5;
+// var totalQuantity = khiraPrice * khiraQuantityinKg;
+// console.log(totalQuantity);
+
+// division in js
+var boroiHalfPrice = boroiPrice / 2;
+console.log(boroiHalfPrice);
