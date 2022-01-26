aadfc92a73458a160ee8c7d4feb8040bc0f695b4
AskatAsh <abdulaskat@gmail.com>
2022-01-26 22:23:45 +0600

    learned string declare and boolean true false

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 var num1= "99.5";
 var num2= 15;
 console.log(num1+num2);
+
+var nam = "amr nam askat";
+var boyos = 'amar boyesh 24 bochor';
+var bari = "amar bari boalkhali";
+
+console.log(nam);
+console.log(boyos);
+console.log(bari);
+
+var valo = true;
+var kharap = false;
+
+console.log(valo);
+console.log(kharap);
