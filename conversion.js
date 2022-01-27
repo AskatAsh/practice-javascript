7778fc04645afecbcaded9937fbe126f1e42ba87
AskatAsh <abdulaskat@gmail.com>
2022-01-27 23:03:24 +0600

    Integer float parseInt parseFloat type conversion learned

--- conversion.js
+++ conversion.js
@@ -11,4 +11,16 @@ var last = '98';
 var full = first+last;
 // console.log(full);
 
+var lastNum = parseInt(last);
+console.log(lastNum);
 
+var riceAmount = '25.550';
+var potatoAmount = '12.899';
+var riceAmountNum = parseFloat(riceAmount);
+var potatoAmountNum = parseFloat(potatoAmount);
+// var totalAmount = riceAmountNum + potatoAmountNum;
+// console.log(totalAmount);
+
+var riceAmountNum = parseInt(riceAmount);
+var totalAmount = riceAmountNum + potatoAmountNum;
+console.log(totalAmount);
