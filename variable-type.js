c48cb1206eac2b76e3e13891796f6e84743f73e2
AskatAsh <abdulaskat@gmail.com>
2022-01-28 02:24:31 +0600

    learned about different variable types

---
+++ variable-type.js
@@ -0,0 +1,15 @@
+var price = 473;
+console.log(typeof price);
+
+var yourName = "askat";
+console.log(typeof yourName);
+
+var isSunny = false;
+console.log(typeof isSunny);
+
+var sampleVar;
+console.log(typeof sampleVar);
+
+var myDate = new Date();
+console.log(myDate);
+console.log(typeof myDate);
