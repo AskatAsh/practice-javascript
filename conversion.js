47f04c4ac9b77157d96145c533ffd8b303f8f386
AskatAsh <abdulaskat@gmail.com>
2022-01-27 22:45:57 +0600

    learned string concatenation

---
+++ conversion.js
@@ -0,0 +1,14 @@
+var firstName = "Abdul";
+var lastName = "Kalam";
+var fullName = firstName + lastName;
+// console.log(fullName);
+
+var fullName = firstName + ' ' + lastName;
+// console.log(fullName);
+
+var first = 23;
+var last = '98';
+var full = first+last;
+// console.log(full);
+
+
