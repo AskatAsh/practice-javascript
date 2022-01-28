4c4355885970adfac50509584527107eb42f4706
AskatAsh <abdulaskat@gmail.com>
2022-01-28 21:08:31 +0600

    Declare Array, array length and array index

---
+++ array.js
@@ -0,0 +1,8 @@
+var friendsId = [67, 99, 70, 65, 52];
+
+var familyMembers = ["father", "mother", "sister", "brother", "nanna", "dadda"];
+
+console.log(friendsId);
+console.log(familyMembers);
+console.log(friendsId.length);
+console.log(familyMembers.length);
