71b27fe0bab3361065eb633e14efb77d3592e020
AskatAsh <abdulaskat@gmail.com>
2022-01-28 23:40:26 +0600

    if else conditionals and uses learned

---
+++ conditionals.js
@@ -0,0 +1,13 @@
+var myMoney = 350;
+var buffetPrice = 600;
+
+// if (myMoney > buffetPrice){
+//   console.log("Let's eat buffet foood....!!!")
+// }
+
+if (buffetPrice < myMoney){
+  console.log("Onek boro restorai iccha moto khabo.")
+}
+else{
+  console.log("Alo vorta painna dail onek moja.")
+}
