d3e4e07c8a2e438f40b824fa970f7903a5da60e3
AskatAsh <abdulaskat@gmail.com>
2022-01-28 22:47:04 +0600

    comparison of operators learned

---
+++ comparison.js
@@ -0,0 +1,22 @@
+// console.log(34 > 65);
+
+// console.log(34 < 65);
+
+// console.log(45 == 45);
+
+// console.log(68 == 45);
+
+// console.log(34 != 34);
+
+// console.log(34 != 45);
+
+var myAge = 24;
+var yourAge = 29;
+
+// console.log(myAge > yourAge);
+// console.log(myAge < yourAge);
+// console.log(myAge == yourAge);
+// console.log(myAge != yourAge);
+
+console.log(isSweet && isTasty); // condition fulfills if both are correct
+console.log(isSweet || isTasty); // condition fulfills if either of them are correct
