97755433e6f8c6a6465a8f61d2ff542287a12bb3
AskatAsh <abdulaskat@gmail.com>
2022-01-28 21:37:04 +0600

    Array index, get and set by index, indexOf learned

--- array.js
+++ array.js
@@ -2,7 +2,29 @@ var friendsId = [67, 99, 70, 65, 52];
 
 var familyMembers = ["father", "mother", "sister", "brother", "nanna", "dadda"];
 
-console.log(friendsId);
-console.log(familyMembers);
-console.log(friendsId.length);
-console.log(familyMembers.length);
+// console.log(friendsId);
+// console.log(familyMembers);
+// console.log(friendsId.length);
+// console.log(familyMembers.length);
+
+var familyPosition = familyMembers.indexOf("brother");
+// console.log(familyPosition);
+
+// var idPosition = friendsId.indexOf(52);
+var idPosition = friendsId.indexOf(152);
+// console.log(idPosition);
+
+// var familyIdentity = familyMembers[5];
+var familyIdentity = familyMembers[6];
+// console.log(familyIdentity);
+
+var friendsIdentity = friendsId[2];
+// console.log(friendsIdentity);
+
+friendsId[4] = 77;
+// console.log(friendsId);
+
+familyMembers[3] = "uncle";
+// console.log(familyMembers);
+
+
