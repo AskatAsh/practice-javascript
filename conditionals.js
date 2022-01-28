11afd174c64fe9bc4abb4e21554c02cd0c6cba51
AskatAsh <abdulaskat@gmail.com>
2022-01-29 01:52:27 +0600

    learned if else conditionals and their uses

--- conditionals.js
+++ conditionals.js
@@ -5,9 +5,27 @@ var buffetPrice = 600;
 //   console.log("Let's eat buffet foood....!!!")
 // }
 
-if (buffetPrice < myMoney){
-  console.log("Onek boro restorai iccha moto khabo.")
+// if (buffetPrice < myMoney){
+//   console.log("Onek boro restorai iccha moto khabo.")
+// }
+// else{
+//   console.log("Alo vorta painna dail onek moja.")
+// }
+
+var outingCondition = false;
+var outingPrice = 52000;
+var outingTimeinHours = 45;
+
+// if (outingCondition == true && outingPrice > 40000){
+//  console.log("cholo amra ghurte jai sent martin dipe..!!!")
+// }
+// else{
+//   console.log("cholo gramer bari beraite jai sent maritn copale nai.")
+// }
+
+if ((outingCondition == true && outingTimeinHours < 40) || outingPrice > 56000){
+ console.log("cholo amra ghurte jai sent martin dipe..!!!")
 }
 else{
-  console.log("Alo vorta painna dail onek moja.")
+  console.log("cholo gramer bari beraite jai sent maritn copale nai.")
 }
