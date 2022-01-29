8d11803d6a0b3c209b70fd25b374bab3c014add7
AskatAsh <abdulaskat@gmail.com>
2022-01-30 01:11:48 +0600

    started learning the basics of function

---
+++ js/function.js
@@ -0,0 +1,10 @@
+var items = 45;
+
+function itemNumPrint(){
+  console.log("This is the number of items: "+items);
+}
+itemNumPrint(); // to execute a function it has to be called by its name
+
+items = items + 45;
+
+itemNumPrint(); // a function can be called many times
