ad87be9422d6c2b041c957e9991d40871c288f67
AskatAsh <abdulaskat@gmail.com>
2022-01-29 19:52:23 +0600

    created a js folder for all the js files

--- script.js
+++ script.js
@@ -12,12 +12,12 @@ var valo = true;
 var kharap = false;
 
 // javascript variable declaration rules:
-var hello world = "welcome to js world";//var name should not have a whitespace
+var helloWorld = "welcome to js world";//var name should not have a whitespace
 
-var 98ash = 1998;// wrong way of adding number in a variable
+// var 98ash = 1998; // wrong way of adding number in a variable
 var ash98 = 1998;// right way
 
-var pasta-time = "evening";// no hyphen allowed in variable
+var pasta_time = "evening";// no hyphen allowed in variable
 var pasta_time = "morning;"// dash is allowed in a variable
 
 var allTextUppercase = "this is called camel casing"// camelcasing is a good practice
