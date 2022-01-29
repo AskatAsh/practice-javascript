ebf389ebcc8fcc28adc6104ea537bd684190d751
AskatAsh <abdulaskat@gmail.com>
2022-01-29 20:56:46 +0600

    learned odd and even numbers code using while loop

--- js/while-loop.js
+++ js/while-loop.js
@@ -1,7 +1,45 @@
-var fuskaMembers = 0;
+// var fuskaMembers = 0;
 
-while (fuskaMembers < 5){
-  console.log(fuskaMembers);
-  console.log("Cholo shobai mile khai fuska...!");
-  fuskaMembers++;
+// while (fuskaMembers < 5){
+//   console.log(fuskaMembers);
+//   console.log("Cholo shobai mile khai fuska...!");
+//   fuskaMembers++;
+// }
+
+// odd numbers
+// var num = 0;
+
+// while(num <= 10){
+//   console.log(num);
+//   num += 2;
+// }
+
+// even numbers
+// num = 1;
+// while (num <= 20){
+//   console.log(num);
+//   num += 2;
+// }
+
+// odd even numbers
+// num = 1;
+// while(num <= 20){
+//   if (num%2 == 0){
+//     console.log(num); // even numbers
+//     num += 2;
+//   }
+//   else{
+//     num++;
+//   }
+// }
+
+num = 1;
+while(num <= 20){
+  if (num%2 != 0){
+    console.log(num); // odd numbers
+    num += 2;
+  }
+  else{
+    num++;
+  }
 }
