0ddc3652847dec2e9d91f4aef10b623190939694
AskatAsh <abdulaskat@gmail.com>
2022-01-29 19:49:01 +0600

    if else and else if consitionals learned and practiced

--- conditionals.js
+++ conditionals.js
@@ -32,22 +32,42 @@ var outingTimeinHours = 45;
 
 var outingVehicle = true;
 
-if (outingCondition == true && outingVehicle == true){
-  if(outingPrice > 51337){
-    console.log("Cholo na ghure asi ojanate...!!!");
+// if (outingCondition == true && outingVehicle == true){
+//   if(outingPrice > 51337){
+//     console.log("Cholo na ghure asi ojanate...!!!");
+//   }
+//   else{
+//     console.log("Cholo gramer bari berate jai");
+//   }
+// }
+// else if(outingCondition == true && outingTimeinHours > 48){
+//   if(outingPrice > 54337 || outingVehicle == true){
+//     console.log("Cholo na ghure asi ojanate...!!!");
+//   }
+//   else{
+//     console.log("Cholo gramer bari berate jai");
+//   }
+// }
+// else{
+//   console.log("Kothao jaoar dorkar nai ghore boshe thako");
+// }
+
+var outingMembers = ['papa', 'mama', 'sister', 'brother', 'dadda', 'nanna', 'uncle', 'aunt'];
+var isRainning = true;
+
+if (outingTimeinHours == 50 || outingVehicle == true){
+  if(outingMembers.length <= 8){
+    console.log("Cholo shobai mile ghurte jai.");
+  }
+  else if(outingPrice >= 45000){
+    console.log("Eto taka khoros kore koi jabo...!");
   }
   else{
-    console.log("Cholo gramer bari berate jai");
-  }
-}
-else if(outingCondition == true && outingTimeinHours > 48){
-  if(outingPrice > 54337 || outingVehicle == true){
-    console.log("Cholo na ghure asi ojanate...!!!");
-  }
-  else{
-    console.log("Cholo gramer bari berate jai");
+    console.log("Cinta kore dekhi kothai jawa jai...")
   }
 }
 else{
-  console.log("Kothao jaoar dorkar nai ghore boshe thako");
+  console.log("Valo lage na ghorei thakbo");
 }
+
+console.log(outingMembers.length);
