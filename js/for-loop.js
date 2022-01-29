cc9f9ce6a64671fa2e5d6f6ca14524558437f7cb
AskatAsh <abdulaskat@gmail.com>
2022-01-29 22:41:00 +0600

    for loop how to use it and while vs for loop learned

---
+++ js/for-loop.js
@@ -0,0 +1,25 @@
+// var num = 1;
+// for(var num = 1; num<=10; num++){
+//   console.log(num);
+// }
+
+// even numbers using for loop
+// for (var i=0; i<=20; i+=2){
+//   console.log(i);
+// }
+
+// odd numbers using for loop
+// for (var i=1; i<=20; i+=2){
+//   console.log(i);
+// }
+
+// even and odd numbers using for loop and if else
+for (var i=3; i<=20; i+=2){
+  if(i%2 != 0){   // if i%2==0 then even number; if i%2==1 then odd numbers
+    console.log("odd numbers : "+i);
+  }
+  else{
+    i++;
+    console.log("odd numbers : "+i);
+  }
+}
