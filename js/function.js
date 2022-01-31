0cede6b590f31e4519938d602a0e963089263c98
AskatAsh <abdulaskat@gmail.com>
2022-01-31 11:54:44 +0600

    learned function parameter and function return

--- js/function.js
+++ js/function.js
@@ -1,10 +1,23 @@
-var items = 45;
+// var items = 45;
 
-function itemNumPrint(){
-  console.log("This is the number of items: "+items);
+// function itemNumPrint(){
+//   console.log("This is the number of items: "+items);
+// }
+// itemNumPrint(); // to execute a function it has to be called by its name
+
+// items = items + 45;
+
+// itemNumPrint(); // a function can be called many times
+
+function chair(taka){ // here moneyIhave is the parameter.
+  // console.log("I have 5 chair in my home");
+  var chairPrice = 500;
+  var chairQuantity = taka / chairPrice;
+  return chairQuantity;
 }
-itemNumPrint(); // to execute a function it has to be called by its name
 
-items = items + 45;
+var moneyIhave = 4500;
+var quantity = chair(moneyIhave);
+// chair(moneyIhave);
 
-itemNumPrint(); // a function can be called many times
+console.log("The number of chairs I have is : " + quantity);
