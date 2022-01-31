7816e140b338639c839f8d51d74c0c81db725d3d
AskatAsh <abdulaskat@gmail.com>
2022-02-01 01:01:44 +0600

    learned multiple parameter using function

---
+++ js/multiple-parameter.js
@@ -0,0 +1,21 @@
+// function of adding two numbers
+// function sumTwoNumbers(number1, number2){
+//   console.log(number1, number2);
+//   var total = number1 + number2;
+//   return total;
+// }
+
+// var firstNumber = 34;
+// var secondNumber = 87;
+// var result = sumTwoNumbers(secondNumber, firstNumber);
+// console.log("The addition of two numbers are : ", result);
+
+// function of multiplying two numbers
+function numMunltiplication(num1, num2){
+  var mul = num1 * num2;
+  return mul;
+}
+var firstNum = 6, secondNum = 50;
+
+var totalMul = numMunltiplication(firstNum, secondNum);
+console.log("Multiplication of two number is : ", totalMul);
