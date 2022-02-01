7b9c09bd57b4dab86fd195c7f5dcd38a7ccd7a30
AskatAsh <abdulaskat@gmail.com>
2022-02-01 17:01:51 +0600

    learned how to declare an object

---
+++ js/object.js
@@ -0,0 +1,14 @@
+var student1 = {
+  name : "rahim",
+  id : 1932067,
+  department : "CSE",
+  semester : "6th"
+};
+
+var laptop = {
+  model : "hp elitebook",
+  generation : "6th",
+  soundSystem : "bang and olufson",
+  ram : "4gb",
+  hardDisk : 500
+};
