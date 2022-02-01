725f5c0a934b6c287e4475af1b86e68f7713323c
AskatAsh <abdulaskat@gmail.com>
2022-02-02 01:59:18 +0600

    learned to use break in for loop and using array

--- js/break-continue.js
+++ js/break-continue.js
@@ -1,7 +1,24 @@
-var i=0;
-while(i<10){
-  if (i==5)
+// var i=0;
+// while(i<10){
+//   if (i==5)
+//   break;
+//   console.log(i);
+//   i++;
+// }
+
+// var i=0;
+// for (var i=0; i<13; i++){
+//   console.log(i);
+//   if (i==7)
+//   break;
+// }
+
+var numbers = [23, 75, 98, 143, 97, 56, 84, 29, 59, 173, 119];
+for (var i=0; i<numbers.length; i++){
+  // console.log(numbers);
+  var num = numbers[i];
+  console.log(num);
+  if (num > 150){
   break;
-  console.log(i);
-  i++;
+  }
 }
