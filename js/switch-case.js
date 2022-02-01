3907a29839209570ed3ad4e5f8d0fd67bb35f653
AskatAsh <abdulaskat@gmail.com>
2022-02-02 00:40:25 +0600

    learned switch case break in js

---
+++ js/switch-case.js
@@ -0,0 +1,28 @@
+// var color = "blue";
+
+// if (color == "red"){
+//   console.log("the selected color is red");
+// }
+// else if (color == "green"){
+//   console.log("the selected color is green");
+// }
+// else if (color == "blue"){
+//   console.log("the selected color is blue");
+// }
+// else {
+//   console.log("the selected color is black");
+// }
+var color = "green";
+switch(color){
+  case "red":
+    console.log("This is color red");
+    break;
+  case "green":
+    console.log("This is color green");
+    break;
+  case "blue":
+    console.log("This is color blue");
+    break;
+  default:
+    console.log("This is color black");
+}
