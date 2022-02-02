9280e1f15b90e8c512a8afd611a2ab6e56dfb32d
AskatAsh <abdulaskat@gmail.com>
2022-02-02 15:13:28 +0600

    learned how to use let and const

---
+++ js/let-const.js
@@ -0,0 +1,7 @@
+let number = [23, 356, 97, 63, 93, 18, 933];
+let numCount = number.length;
+console.log(numCount);
+
+const name = "laptop hp"; //constant varaiable is unchangable
+// name = "hp elitebook"; // cannot be declared because name is a constant variable
+console.log(name);
