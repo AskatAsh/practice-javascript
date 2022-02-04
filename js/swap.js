d727bb45932f7a5d5487e3c327a850518587ba83
AskatAsh <abdulaskat@gmail.com>
2022-02-04 22:29:13 +0600

    learned swapping without temp using destructuring

--- js/swap.js
+++ js/swap.js
@@ -3,7 +3,12 @@ let second = 78;
 console.log(first, second);
 
 // swapping two variables
-let temp = first;
-first = second;
-second = temp;
-console.log(first, second); // swap using tamporary variables
+// let temp = first;
+// first = second;
+// second = temp;
+// console.log(first, second); // swap using tamporary variables
+
+// swapping without temp - destructuring
+[first, second] = [second, first]; // destructuring array;
+console.log(first, second);
+
