48d0bd5086f590186a4ffd86e466183d6668bf15
AskatAsh <abdulaskat@gmail.com>
2022-02-04 23:55:29 +0600

    finding biggest number of three values

---
+++ js/max-min.js
@@ -0,0 +1,13 @@
+let firstNum = 743;
+let secondNum = 733;
+let thirdNum = 593;
+
+if (firstNum>secondNum && firstNum>thirdNum){
+  console.log("the first number is bigger:",firstNum);
+}
+else if (secondNum>firstNum && secondNum>thirdNum){
+  console.log("the second number is bigger:",secondNum);
+}
+else {
+  console.log("the third number is bigger:",thirdNum);
+}
