e105e57fd689df6bbace5d934b5ad1cf8aefdb9e
AskatAsh <abdulaskat@gmail.com>
2022-02-05 02:44:54 +0600

    learned fobonacci in function and Handle unexpected input using simple return

--- js/fibonacci.js
+++ js/fibonacci.js
@@ -1,5 +1,22 @@
-const fibona = [0, 1];
-for (let i=2; i<=10; i++){
-  fibona[i] = fibona[i-1] + fibona[i-2];
+// const fibona = [0, 1];
+// for (let i=2; i<=10; i++){
+//   fibona[i] = fibona[i-1] + fibona[i-2];
+// }
+// console.log(fibona);
+
+function fibonacci(fiboNum) {
+  if (typeof fiboNum != "number"){
+    return "please give a valid number";
+  }
+  else if (fiboNum < 2){
+    return "please give a positive number";
+  }
+  const fibona = [0, 1];
+  for (let i = 2; i < fiboNum; i++) {
+    fibona[i] = fibona[i - 1] + fibona[i - 2];
+  }
+  return fibona;
 }
-console.log(fibona);
+let numInFibo = 14;
+let result = fibonacci(numInFibo);
+console.log(result);
