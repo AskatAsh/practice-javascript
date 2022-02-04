b6ffee091f97b70ba316d947cab31e7323efcd3a
AskatAsh <abdulaskat@gmail.com>
2022-02-04 22:13:27 +0600

    learned swapping using temporary variables

---
+++ js/swap.js
@@ -0,0 +1,9 @@
+let first = 34;
+let second = 78;
+console.log(first, second);
+
+// swapping two variables
+let temp = first;
+first = second;
+second = temp;
+console.log(first, second); // swap using tamporary variables
