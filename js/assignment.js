08ddc814f2b3ae1dddd46cc188e23cb86e36c9ec
AskatAsh <abdulaskat@gmail.com>
2022-02-06 00:45:58 +0600

    assignment problem panda cost is updated

--- js/assignment.js
+++ js/assignment.js
@@ -1,31 +1,77 @@
 // problem 1: ana to vori
 
-// function anaToVori(ana){
-//   let vori = ana/16;
-//   return vori;
-// }
-// let jotoAna = 32;
-// let totoVori = anaToVori(jotoAna);
-// console.log(jotoAna + " ana to vori is : "+totoVori);
-
-
+function anaToVori(ana){
+  let vori = ana/16;
+  return vori;
+}
+let jotoAna = 32;
+let totoVori = anaToVori(jotoAna);
+console.log(jotoAna + " ana to vori is : "+totoVori);
 
 
 // problem 2: panda cost
-function pandaCost(singara, somusa, jilapi){
+function pandaCost(singara, somusa, jilapi) {
   const singaraPrice = 7;
   const somusaPrice = 10;
   const jilapiPrice = 15;
+  if (singara > 0 && somusa > 0 && jilapi > 0) {
+    let totalsingaraPrice = singara * singaraPrice;
+    let totalsomusaPrice = somusa * somusaPrice;
+    let totaljilapiPrice = jilapi * jilapiPrice;
 
-  let totalsingaraPrice = singara * singaraPrice;
-  let totalsomusaPrice = somusa * somusaPrice;
-  let totaljilapiPrice = jilapi * jilapiPrice;
+    let totalPandaCost =
+      totalsingaraPrice + totalsomusaPrice + totaljilapiPrice;
+    return totalPandaCost;
+  } else if (singara > 0 && somusa > 0 && jilapi <= 0) {
+    let totalsingaraPrice = singara * singaraPrice;
+    let totalsomusaPrice = somusa * somusaPrice;
+    // let totaljilapiPrice = jilapi * jilapiPrice;
 
-  let totalPandaCost = totalsingaraPrice+totalsomusaPrice+totaljilapiPrice;
-  return totalPandaCost;
+    let totalPandaCost = totalsingaraPrice + totalsomusaPrice;
+    return totalPandaCost;
+  } else if (singara > 0 && somusa <= 0 && jilapi > 0) {
+    let totalsingaraPrice = singara * singaraPrice;
+    // let totalsomusaPrice = somusa * somusaPrice;
+    let totaljilapiPrice = jilapi * jilapiPrice;
+
+    let totalPandaCost = totalsingaraPrice + totaljilapiPrice;
+    return totalPandaCost;
+  } else if (singara <= 0 && somusa > 0 && jilapi > 0) {
+    // let totalsingaraPrice = singara * singaraPrice;
+    let totalsomusaPrice = somusa * somusaPrice;
+    let totaljilapiPrice = jilapi * jilapiPrice;
+
+    let totalPandaCost = totalsomusaPrice + totaljilapiPrice;
+    return totalPandaCost;
+  } else if (singara > 0 && somusa <= 0 && jilapi <= 0) {
+    let totalsingaraPrice = singara * singaraPrice;
+    // let totalsomusaPrice = somusa * somusaPrice;
+    // let totaljilapiPrice = jilapi * jilapiPrice;
+
+    let totalPandaCost = totalsingaraPrice;
+    return totalPandaCost;
+  } 
+  else if (singara <= 0 && somusa > 0 && jilapi <= 0) {
+    // let totalsingaraPrice = singara * singaraPrice;
+    let totalsomusaPrice = somusa * somusaPrice;
+    // let totaljilapiPrice = jilapi * jilapiPrice;
+
+    let totalPandaCost = totalsomusaPrice;
+    return totalPandaCost;
+  } 
+  else if (singara <= 0 && somusa <= 0 && jilapi > 0){
+    let totaljilapiPrice = jilapi * jilapiPrice;
+
+    let totalPandaCost = totaljilapiPrice;
+    return totalPandaCost;
+  } 
+  else {
+    return undefined;
+  }
 }
-let singaraNum = 2;
+let singaraNum = 0;
 let somusaNum = 3;
 let jilapiNum = 4;
 let totalCost = pandaCost(singaraNum, somusaNum, jilapiNum);
 console.log("Total panda cost is :", totalCost);
+
