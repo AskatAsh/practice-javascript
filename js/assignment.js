831ffc0145fe0b3bec905cd6c2f311054bb7b91d
AskatAsh <abdulaskat@gmail.com>
2022-02-05 20:10:29 +0600

    assignment problem ana to vori is solved

---
+++ js/assignment.js
@@ -0,0 +1,9 @@
+// problem 1: ana to vori
+
+function anaToVori(ana){
+  let vori = ana/16;
+  return vori;
+}
+let jotoAna = 32;
+let totoVori = anaToVori(jotoAna);
+console.log(jotoAna + " ana to vori is : "+totoVori);
