35e74c9127848a06496d5c6af11a2982dc0607fb
AskatAsh <abdulaskat@gmail.com>
2022-02-05 15:41:48 +0600

    learned to find even numbers in an array

--- js/array-problems.js
+++ js/array-problems.js
@@ -15,7 +15,7 @@ console.log(result); */
 
 
 // finding the smallest element in an array
-function arrayMin(arrayNum) {
+/* function arrayMin(arrayNum) {
   let min = arrayNum[0];
   for (let i = 0; i < arrayNum.length; i++) {
     const element = arrayNum[i];
@@ -27,4 +27,19 @@ function arrayMin(arrayNum) {
 }
 let numbers = [34, 74, 83, 28, 93, 48, 58, 68];
 let result = arrayMin(numbers);
-console.log(result);
+console.log(result); */
+
+
+// function to find even numbers in an array
+function findEvenNum(){
+  let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+  // let even;
+  for (let i=0; i<arr.length; i++){
+    if (arr[i] % 2 == 0){
+      console.log(arr[i]);
+    }
+  }
+}
+// let result = findEvenNum();
+// console.log(result);
+console.log(findEvenNum());
