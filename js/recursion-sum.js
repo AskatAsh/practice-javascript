0ac1aa494badcd2e8c9484d154a3aac3e787de16
AskatAsh <abdulaskat@gmail.com>
2022-02-09 21:59:52 +0600

    learned differnece between pre and post increament decreament

--- js/recursion-sum.js
+++ js/recursion-sum.js
@@ -1,7 +1,17 @@
 // simple sum of numbers using for loop
-let sum = 0;
-for (let i=5; i>=0; i--){ // i>=1 works the same;
-  // console.log(i);
-  sum = sum + i;
+// let sum = 0;
+// for (let i=5; i>=0; i--){ // i>=1 works the same;
+//   // console.log(i);
+//   sum = sum + i;
+// }
+// console.log("sum =",sum);
+
+
+// sum of numbers using recursion
+function sum(i){
+  console.log(i);
+  return i + sum(i--); 
+  // --i decrements the value, then evaluates and stores it.
+  // i-- evaluates the value, then decrements and stores it.
 }
-console.log("sum =",sum);
+console.log(sum(5));
